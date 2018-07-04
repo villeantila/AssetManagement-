d5645768597c4dd4a0ec9054a0e2d7eacaf2438d
Ville Antila <ville.antila@stud.pointcollege.fi>
2018-07-04 11:58:17 +0300

    ASP.NET MVC -projektin luonti.

---
+++ AssetManagementWeb/AssetManagementWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AssetManagementWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AssetManagementWeb/AssetManagementWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssetManagementWeb.MvcApplication" Language="C#" %>
